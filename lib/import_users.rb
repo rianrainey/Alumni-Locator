5fc0632bfad635e08147a9a0d601f2d771f3c0fb
Rian Rainey <rian@rianrainey.com>
2013-12-11 21:48:25 -0600

    Removed Pry requirement and extraneous code. Also removed vendor/plugins.

--- lib/import_users.rb
+++ lib/import_users.rb
@@ -1,6 +1,5 @@
 module ImportUsers
   def self.import(uploaded_file)
-    require 'pry'
     require 'roo'
 
     fu = FileUtility.new
@@ -37,7 +36,6 @@ module ImportUsers
     end
 
     def get_spreadsheet(filepath, extension="xls")
-      binding.pry
       case extension
       when "xlsx"
         spreadsheet = Roo::Excelx.new(filepath, nil, :ignore)
@@ -66,58 +64,3 @@ module ImportUsers
     end
   end
 end
-
-
-#namespace :import do
-#
-  #task :scholars, [:filepath] => [:environment] do |s, args|
-
-    #spreadsheet = Roo::Spreadsheet.open(args[:filepath])
-    #column_count = spreadsheet.last_column
-
-    #2.upto(spreadsheet.last_row) do |row|
-
-      #email  = spreadsheet.cell(row, 11)
-
-      #@user = User.find_by_email(email)
-      #@user ? @contact_info = @user.contact_info : @contact_info = ContactInfo.new
-
-      #if @user
-        #info = get_user_info(spreadsheet, row, @contact_info)
-
-        #@user.update_attributes(contact_info: @contact_info)
-
-      #else
-        #info = get_user_info(spreadsheet, row, @contact_info)
-
-        #User.create(email: email,
-                    #password: "#{info.first_name.to_s.downcase}-#{info.last_name.to_s.downcase}",
-                    #password_confirmation: "#{info.first_name.to_s.downcase}-#{info.last_name.to_s.downcase}",
-                    #contact_info: @contact_info )
-      #end
-
-
-
-
-    #end
-
-  #end
-#end
-
-#def get_user_info(spreadsheet, row, info)
-  #info.first_name           = spreadsheet.cell(row, 1)
-  #info.last_name            = spreadsheet.cell(row, 2)
-  #info.undergrad_school     = spreadsheet.cell(row, 3)
-  #info.undergrad_grad_year  = spreadsheet.cell(row, 4)
-  #info.home_address         = spreadsheet.cell(row, 5)
-  #info.home_address_2       = spreadsheet.cell(row, 6)
-  #info.home_city            = spreadsheet.cell(row, 7)
-  #info.home_state           = spreadsheet.cell(row, 8)
-  #info.home_zip_code        = spreadsheet.cell(row, 9)
-  #info.phone_main           = spreadsheet.cell(row, 10)
-  #info
-#end
-
-
-
-
